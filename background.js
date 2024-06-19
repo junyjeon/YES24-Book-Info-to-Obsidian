29709b338100462cb8cf0b6a69b065982ac5d05b
junyjeon <jjy8479@gmail.com>
2024-06-19 14:14:26 +0900

    feat: 책 정보 파싱 후 JSON 파일로 저장하기

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"],
-  });
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      files: ["content.js"],
+    },
+    () => {
+      // content.js 실행 후 옵시디언 플러그인으로 메시지 보내기
+      chrome.tabs.sendMessage(tab.id, { action: "createMarkdownFile" });
+    }
+  );
 });
--- content.js
+++ content.js
@@ -7,10 +7,10 @@ function parseBookInfo() {
   // 이미지 URL
   const imageSrc = document.querySelector(".gImg").src;
   // 저자 정보
-  const authorElements = document.querySelectorAll('.gd_auth a');
-  const authors = Array.from(authorElements).map(author => ({
+  const authorElements = document.querySelectorAll(".gd_auth a");
+  const authors = Array.from(authorElements).map((author) => ({
     name: author.innerText,
-    link: author.href
+    link: author.href,
   }));
   // 도서 제본 방식
   const featureElement = document.querySelector(".gd_feature");
@@ -19,31 +19,51 @@ function parseBookInfo() {
   const rating = document.querySelector("#spanGdRating .yes_b").innerText;
 
   // 리뷰 수
-  const reviewCount = document.querySelector(".gd_reviewCount .txC_blue").innerText;
-  
+  const reviewCount = document.querySelector(
+    ".gd_reviewCount .txC_blue"
+  ).innerText;
+
   // 출판사
   const publisher = document.querySelector(".gd_pub a").innerText;
   // XPath를 사용하여 특정 텍스트를 포함하는 th 요소를 찾고, 그 다음 형제 요소인 td 요소를 가져옴
   const getTextByXPath = (xpath) => {
-    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+    const result = document.evaluate(
+      xpath,
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    );
     return result.singleNodeValue ? result.singleNodeValue.innerText : "";
   };
 
   // ISBN
-  const isbn13 = getTextByXPath('//th[contains(text(), "ISBN13")]/following-sibling::td');
-  const isbn10 = getTextByXPath('//th[contains(text(), "ISBN10")]/following-sibling::td');
-  
+  const isbn13 = getTextByXPath(
+    '//th[contains(text(), "ISBN13")]/following-sibling::td'
+  );
+  const isbn10 = getTextByXPath(
+    '//th[contains(text(), "ISBN10")]/following-sibling::td'
+  );
+
   // 페이지 수, 무게, 크기
-  const pageInfo = getTextByXPath('//th[contains(text(), "쪽수, 무게, 크기")]/following-sibling::td');
-  
+  const pageInfo = getTextByXPath(
+    '//th[contains(text(), "쪽수, 무게, 크기")]/following-sibling::td'
+  );
+
   // 정가
-  const listPrice = getTextByXPath('//th[contains(text(), "정가")]/following-sibling::td').trim();
-  
+  const listPrice = getTextByXPath(
+    '//th[contains(text(), "정가")]/following-sibling::td'
+  ).trim();
+
   // 판매가
-  const salePrice = getTextByXPath('//th[contains(text(), "판매가")]/following-sibling::td').trim();
-  
+  const salePrice = getTextByXPath(
+    '//th[contains(text(), "판매가")]/following-sibling::td'
+  ).trim();
+
   // YES포인트 적립
-  const yesPointReward = getTextByXPath('//th[contains(text(), "YES포인트")]/following-sibling::td').trim();
+  const yesPointReward = getTextByXPath(
+    '//th[contains(text(), "YES포인트")]/following-sibling::td'
+  ).trim();
 
   // 판매지수
   const salesPointText = document.querySelector(".gd_sellNum").innerText.trim();
@@ -56,52 +76,67 @@ function parseBookInfo() {
   const publishDate = document.querySelector(".gd_date").innerText;
 
   // 카테고리 정보를 파싱하는 부분
-  const categoryElements = document.querySelectorAll('dl.yesAlertDl li');
-  const categories = Array.from(categoryElements).map(li => {
-    return Array.from(li.querySelectorAll('a')).map(a => a.innerText.trim()).join(" > ");
+  const categoryElements = document.querySelectorAll("dl.yesAlertDl li");
+  const categories = Array.from(categoryElements).map((li) => {
+    return Array.from(li.querySelectorAll("a"))
+      .map((a) => a.innerText.trim())
+      .join(" > ");
   });
 
   // 파싱한 데이터를 객체로 구성
   const bookData = {
+    // 책의 기본 정보
     title: {
       mainTitle: mainTitle, // 메인 제목
       subTitle: subTitle, // 부제목
     },
     imageSrc, // 이미지 URL
+
+    // 저자 정보
     authors, // 저자 정보
-    
-    rating, // 리뷰 총점
-    reviewCount, // 리뷰 수
-    
-    publishDate, // 발행일
+
+    // 출판 정보
     publisher, // 출판사
-    
+    publishDate, // 발행일
+
+    // ISBN 정보
     isbn: {
       isbn13, // ISBN13
       isbn10, // ISBN10
     },
+
+    // 페이지 정보
     pageInfo, // 페이지 수, 무게, 크기
-    
+
+    // 가격 정보
     price: {
       listPrice, // 정가
       salePrice, // 판매가
       yesPointReward, // YES포인트
     },
-    
+
+    // 리뷰 및 평점 정보
+    rating, // 리뷰 총점
+    reviewCount, // 리뷰 수
+
+    // 판매 정보
     salesPoint, // 판매지수
     bestRank, // 베스트 순위
+
+    // 카테고리 정보
     categories, // 카테고리
   };
-  console.log("Sending bookData to background.js:", bookData);
-  
-  // 여기서 파싱한 정보를 옵시디언으로 전송하는 로직을 추가
+
+  // JSON 데이터를 파일로 저장
+  const blob = new Blob([JSON.stringify(bookData)], {
+    type: "application/json",
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "bookData.json";
+  a.click();
+  URL.revokeObjectURL(url);
 }
-// console.log(bookData);
-console.log("test");
 
 parseBookInfo();
-
-// // 페이지 로드 완료 시 책 정보 파싱
-// window.addEventListener("load", parseBookInfo);
-// // DOMContentLoaded 이벤트를 사용하여 페이지 로드 완료 시 책 정보 파싱
-// document.addEventListener("DOMContentLoaded", parseBookInfo);
